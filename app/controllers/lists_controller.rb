d29cad1f3afe7da84f5c309b2301adbf71592bbd
Flavie Leroy <leroy.flavie@outlook.fr>
2021-06-10 16:55:35 +0200

    bug

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,10 +3,27 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-
   def new
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
